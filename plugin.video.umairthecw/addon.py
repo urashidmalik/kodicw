04a0c4788867c1702be9d104b48367c0372a8e91
Umair Malik <urashidmalik@gmail.com>
2015-03-11 02:20:37 -0700

    Folder Struct

--- plugin.video.umairthecw/addon.py
+++ plugin.video.umairthecw/addon.py
@@ -1,5 +1,7 @@
 import base64
 import urllib2
+import urllib
+import urlparse
 import re
 import sys
 import xbmcgui
@@ -85,12 +87,33 @@ def get_last_five_episodes(show_dict):
 #m3u8_obj = m3u8.load("http://www.cwtv.com/shows/arrow")
 #print( m3u8_obj.segments)
 
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
 
-xbmcplugin.endOfDirectory(addon_handle)
+mode = args.get('mode', None)
+
+if mode is None:
+    url = build_url({'mode': 'folder', 'foldername': 'Folder One'})
+    li = xbmcgui.ListItem('Folder One', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    url = build_url({'mode': 'folder', 'foldername': 'Folder Two'})
+    li = xbmcgui.ListItem('Folder Two', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'folder':
+    foldername = args['foldername'][0]
+    url = 'http://localhost/some_video.mkv'
+    li = xbmcgui.ListItem(foldername + ' Video', iconImage='DefaultVideo.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
