63983a4077267b51a213cd6ed351f6178676d809
Umair Malik <urashidmalik@gmail.com>
2015-03-12 01:51:46 -0700

    Sub Directory working. URL mapping sorted

--- plugin.video.umairthecw/addon.py
+++ plugin.video.umairthecw/addon.py
@@ -12,36 +12,44 @@ current_show_list = cwtv.get_shows_list()
 
 xbmcplugin.setContent(addon_handle, 'cwtv')
 
-
+def get_show_dict_by_id(id):
+    for current_show in current_show_list:
+        if current_show['id'] == id:
+            assert isinstance(current_show, object)
+            return current_show
 
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 def add_shows():
     for current_show in current_show_list:
-        url = build_url(current_show)
+        url = build_url({'mode': 'show', 'id': current_show['id']})
+
         li = xbmcgui.ListItem(current_show['name'], iconImage=current_show['image'])
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
                                     listitem=li, isFolder=True)
     xbmcplugin.endOfDirectory(addon_handle)
 
-def add_episode():
-    for current_show in current_show_list:
-        current_episode_list = cwtv.get_last_five_episodes(current_show)
-        url1 = build_url(current_show)
-        for current_episode in current_episode_list:
-            url = build_url(current_episode)
-            li = xbmcgui.ListItem(current_episode['name'], iconImage=current_episode['image'])
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url1,
-                                        listitem=li, isFolder=True)
-        xbmcplugin.endOfDirectory(addon_handle)
+def add_episode(id):
+    current_episode_list = cwtv.get_last_five_episodes(get_show_dict_by_id(id))
+
+    for current_episode in current_episode_list:
+        url = build_url({'mode': 'episode', 'id': current_episode['id']})
+        li = xbmcgui.ListItem(current_episode['name'], iconImage=current_episode['image'])
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                    listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+print args
 
 mode = args.get('mode', None)
 if mode is None:
     add_shows()
 elif mode[0] == 'show':
-    add_episode()
-    pass
+    print 'Adding Episodes'
+    add_episode(args.get('id')[0])
+
 
 # if mode is None:
 #     url = build_url({'mode': 'folder', 'foldername': 'Folder One'})
