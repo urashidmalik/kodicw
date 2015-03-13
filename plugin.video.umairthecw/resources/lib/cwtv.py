50484633ca7d75a60fd533a255f2ecf4733fd8be
Umair Malik <urashidmalik@gmail.com>
2015-03-13 03:34:38 -0700

    Work in progress to get video data

--- plugin.video.umairthecw/resources/lib/cwtv.py
+++ plugin.video.umairthecw/resources/lib/cwtv.py
@@ -2,9 +2,12 @@ __author__ = 'umair'
 import base64
 import urllib2
 import re
-#import m3u8
+import json
+
+import m3u8
 from pprint import  pprint
 
+video_data_json = "http://metaframe.digitalsmiths.tv/v2/CWtv/assets/%s/partner/132?format=json"
 
 url_dict = {
     "main": "http://www.cwtv.com",
@@ -21,8 +24,11 @@ def __get_cw_page_(url, ref_url='http://www.cwtv.com/shows/the-flash/'):
     :return: Page Data in Text
     """
     req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
-    req.add_header('Referer', base64.b64decode(ref_url))
+    #req.add_header('User-Agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0')
+
+
+    #req.add_header('Referer', base64.b64decode(ref_url))
 
     try:
         response = urllib2.urlopen(req)
@@ -69,14 +75,17 @@ def get_last_five_episodes(show_dict):
     match = re.compile('<ul id=\"list_1\"((.|\n)*?)<\/ul>').findall(current_episodes)
     if match == []:
         return []
+    media_key_match = re.compile('<a class="thumbLink" href=".*play=(.*)">').findall(match[0][0])
     img_match = re.compile('img src="(.*)"').findall(match[0][0])
     name_match = re.compile('videodetails1.*\n.*<p>(.*)<').findall(match[0][0])
     url_match = re.compile('<a class="thumbLink" href="(.*)">').findall(match[0][0])
     episode_list = []
     i = 0
     for name in name_match :
-        episode_dict = dict(mode='episode', image=img_match[i], name=name_match[i], url=url_dict.get("main") + url_match[i],
-                            id=name_match[i].replace(" ", ""))
+        episode_dict = dict(mode='episode', image=img_match[i], name=name_match[i],
+                            url=url_dict.get("main") + url_match[i],
+                            id=name_match[i].replace(" ", ""),
+                            video_data=video_data_json % media_key_match[i])
         i += 1
         episode_list.append(episode_dict)
     return episode_list
@@ -93,11 +102,34 @@ def get_show_dict_by_id(id):
             assert isinstance(current_show, object)
             return current_show
 ###############################################################################################
+def get_m3u8_link_for_video(link):
 
-#allShows = get_shows_list()
+
+    json_str =  __get_cw_page_(link)
+    json_acceptable_string = json_str.replace("'", "\"")
+    json_data = json.loads(json_acceptable_string)
+    playlist_m3u8 = json_data['videos']['variantplaylist']['uri']
+    m3u8_data = __get_cw_page_(playlist_m3u8)
+    current =  0
+    highest = 0
+    playlist = None
+    m3u8_obj = m3u8.parse(m3u8_data)
+    for video_index in m3u8_obj.get('playlists'):
+       current =  video_index.get('stream_info')['bandwidth']
+       if(current > highest):
+           highest = current
+           playlist = video_index.get('uri')
+    return playlist
+
+###############################################################################################
+
+###############################################################################################
+
+allShows = get_shows_list()
 #pprint(allShows[4])
-#allEpisodes = get_last_five_episodes(allShows[8])
-#pprint(allEpisodes)
+allEpisodes = get_last_five_episodes(allShows[8])
+#pprint(allEpisodes[0]['video_data'])
+print get_m3u8_link_for_video(allEpisodes[0]['video_data'])
 #m3u8_obj = m3u8.load("http://hlsioscwtv.warnerbros.com/hls/2015/02/24/Arrow-315-NandaParbat-3J5165-CW-Stereo_a12c4752a_2100kbps/Arrow-315-NandaParbat-3J5165-CW-Stereo_a12c4752a_2100kbps.m3u8");
 #m3u8_obj = m3u8.load("http://www.cwtv.com/shows/arrow")
 #print( m3u8_obj.segments)
